2c493fafc93792c1f4123726b4e991cb237a6941
moi9175013 <boy.jdn@free.fr>
2024-12-28 17:22:24 +0100

    Update photo.php

--- photo.php
+++ photo.php
@@ -4,16 +4,16 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// R�cup�rer les donn�es envoy�es en POST
+// Recuperer les donnees envoyees en POST
 $latitude = $_POST['latitude'];
 $longitude = $_POST['longitude'];
 
-// Sauvegarder les donn�es dans un fichier (par exemple)
+// Sauvegarder les donnees dans un fichier (par exemple)
 $file = 'localisations.txt';
 $current = file_get_contents($file);
 $current .= "Latitude: $latitude, Longitude: $longitude\n";
 file_put_contents($file, $current);
 
-// Optionnel : r�ponse � retourner au client
+// Optionnel : reponse et retourner au client
 
 ?>
